071ff07529104985e8a20faff4df0dc47fe3ef6d
willscottrod <hello@wsr.nz>
2025-09-02 14:30:57 +1200

    fix: :bug: fix favicon and styling

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -55,6 +55,6 @@ export const metadata: Metadata = {
   openGraph: mergeOpenGraph(),
   twitter: {
     card: 'summary_large_image',
-    creator: '@payloadcms',
+    creator: 'Pioneer Finance',
   },
 }
--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'EASYCARD TO CONTINUE',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/website-template-OG.png`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Easycard',
+  title: 'Easycard',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
